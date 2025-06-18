a622ab2378d0032968fa3462723ab3b02c27b463
Wajed Isleem <wajedbisleem@gmail.com>
2025-06-18 03:45:55 -0600

    Update welcome message in HomeController to reflect app template

--- template/controllers/home.controller.js
+++ template/controllers/home.controller.js
@@ -1,6 +1,6 @@
 class HomeController {
   static index(req, res) {
-    return res.send('Welcome to the Node.js API Template!');
+    return res.send('Welcome to the Node.js App Template!');
   }
 }
 
